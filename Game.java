6ad577ddbded3d3687af985bde769c997fcefc69
unknown <juanjosealzateescudero@gmail.com>
2025-05-16 18:54:39 -0500

    2nd Version

---
+++ Game.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Game {
+    int Intentos = 15;
+    int NumeroMaximo= 50;
+    int IntentosRestantes;
+    int Numero = random.nextInt(50) + 1;
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("JUEGO DE ADIVINANZA");
+    System.out.println("El número máximo de intentos para adivinar el número es " + Intentos);
+    System.out.println("Debes adivinar un número que está entre 1 y 50 " + NumeroMaximo);
+    
+}

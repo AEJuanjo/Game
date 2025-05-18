23d71fa1109bced9d634ddf4827f8fe8b5cd40d9
unknown <juanjosealzateescudero@gmail.com>
2025-05-17 22:12:36 -0500

    4th Version

--- Game.java
+++ Game.java
@@ -5,7 +5,8 @@ public class Game {
     public static void main(String[] args){
         int Intentos = 15;
         int NumeroMaximo= 50;
-        int IntentosRestantes;
+        int IntentosRestantes = Intentos;
+        Random random = new Random();
         int Numero = random.nextInt(50) + 1;
         Scanner scanner = new Scanner(System.in);
 
@@ -19,9 +20,25 @@ public class Game {
         System.out.println("Introduce un número entre 1 y 50");
         int Adivina = scanner.nextInt();
         
-        if (Adivinanza == Numero) {
+        if (Adivina == Numero) {
             System.out.println("Has adivinado el número.");
-            
+        }
+        else if (Adivina < Numero) {
+            System.out.println("El número es mayor.");
+        } else {
+            System.out.println("El número es menor.");
+        }
+
+        System.out.println("Te quedan " + (Intentos - IntentosRestantes) + " intentos.");
+        IntentosRestantes--;
+        if (IntentosRestantes == 0) {
+            System.out.println("Has perdido. El número era " + Numero);
+        }
+        
+        scanner.close();
+    }
+}
+
 
         
-}    
+    

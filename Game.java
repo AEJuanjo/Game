2788eb219b7c4aa33207e63f107fa794b6f2d560
unknown <juanjosealzateescudero@gmail.com>
2025-05-17 22:24:35 -0500

    5th Version

--- Game.java
+++ Game.java
@@ -4,37 +4,39 @@ import java.util.Random;
 public class Game {
     public static void main(String[] args){
         int Intentos = 15;
-        int NumeroMaximo= 50;
+        int NumeroMaximo = 50;
         int IntentosRestantes = Intentos;
         Random random = new Random();
-        int Numero = random.nextInt(50) + 1;
+        int Numero = random.nextInt(NumeroMaximo) + 1;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("JUEGO DE ADIVINANZA");
-        System.out.println("El número máximo de intentos para adivinar el número es " + Intentos);
-        System.out.println("Debes adivinar un número que está entre 1 y 50 " + NumeroMaximo);
-    
-    
-        while (IntentosRestantes < Intentos) {
-        System.out.println("Tienes " + IntentosRestantes + "intentos restantes");}
-        System.out.println("Introduce un número entre 1 y 50");
-        int Adivina = scanner.nextInt();
-        
-        if (Adivina == Numero) {
-            System.out.println("Has adivinado el número.");
-        }
-        else if (Adivina < Numero) {
-            System.out.println("El número es mayor.");
-        } else {
-            System.out.println("El número es menor.");
+        System.out.println("El numero maximo de intentos para adivinar el numero es " + Intentos);
+        System.out.println("Debes adivinar un numero que esta entre 1 y " + NumeroMaximo);
+
+        while (IntentosRestantes > 0) {
+            System.out.println("Tienes " + IntentosRestantes + " intentos restantes");
+            System.out.print("Introduce un numero entre 1 y " + NumeroMaximo + ": ");
+            int Adivina = scanner.nextInt();
+
+            if (Adivina == Numero) {
+                System.out.println("¡Has adivinado el número!");
+                break; // Termina el juego si adivina
+            }
+            else if (Adivina < Numero) {
+                System.out.println("El numero es mayor.");
+            } else {
+                System.out.println("El numero es menor.");
+            }
+
+            IntentosRestantes--;
+
+            if (IntentosRestantes == 0) {
+                System.out.println("Has perdido. El numero era " + Numero);
+            }
         }
 
-        System.out.println("Te quedan " + (Intentos - IntentosRestantes) + " intentos.");
-        IntentosRestantes--;
-        if (IntentosRestantes == 0) {
-            System.out.println("Has perdido. El número era " + Numero);
-        }
-        
+        System.out.println("Gracias por jugar.");
         scanner.close();
     }
 }
